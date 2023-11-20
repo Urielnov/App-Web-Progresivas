50238abccdad1565c36dda326203308ac930b88c
Urielnov <98472922+Urielnov@users.noreply.github.com>
2023-11-20 14:54:42 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,82 @@
+//Asignar nombre y version de la cache
+const CACHE_NAME = 'v1_cache_UrielGarciaRamirez';
+
+//ficheros a cachear en la aplicacion
+var urlsToCache = [
+    './',
+    './CSS/styles.css',
+    './img/ugr.png',
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/facebook.png',
+    './img/instagram.png',
+    './img/twitter.png',
+    '/img/ugr1-1024.png',
+    '/img/ugr2-512.png',
+    '/img/ugr3-384.png',
+    '/img/ugr4-256.png',
+    '/img/ugr5-192.png',
+    '/img/ugr6-128.png',
+    '/img/ugr7-96.png',
+    '/img/ugr8-64.png',
+    '/img/ugr9-32.png',
+    '/img/ugr10-16.png'
+];
+
+//Evento install
+//Instalacion del service Worker y guarda en cache los recursos es
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache)
+            .then(()=> {
+                self.skipWaiting();
+            });
+        })
+        .catch(err => console.log('No se a registrado el cache', err))
+    );
+});
+
+//Evento activate
+//que la app funciones sin conexion
+self.addEventListener('activate', e => {
+    const cacheWhitelist =[CACHE_NAME];
+
+    e.waitUntil(
+        caches.keys()
+        .then(cacheName => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if(cacheWhitelist.indexOf(cacheName) === -1){
+                        //Borrar elementos que no se necesitan
+                        return caches.delete(cacheName);
+                    }
+
+                })
+            );
+        })
+        .then(() => {
+            //Activar cache
+            self.clients.claim();
+        })
+    );
+});
+
+//Evento fetch
+self.addEventListener('fetch', e =>{
+
+    e.respondWith(
+        caches.match(e.request)
+        .then(res =>{
+            if(res){
+                return res;
+            }
+            return fetch(e.request);
+        })
+    );
+});
